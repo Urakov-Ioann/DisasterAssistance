1ecabb2d2ec6de3c69c78135e165585760a44a25
Ioann Urakov <urakovdfyz5@yandex.ru>
2024-04-20 13:41:14 +0300

    Initial Commit

---
+++ DisasterAssistance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisasterAssistance
+//
+//  Created by Иоанн Ураков on 20.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
