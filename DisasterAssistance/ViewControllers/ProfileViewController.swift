7425824181974a3f66b6ffaf84ca8b9f3d87af56
Ioann Urakov <urakovdfyz5@yandex.ru>
2024-04-22 23:50:35 +0300

    Add tableView to profile

--- DisasterAssistance/ViewControllers/ProfileViewController.swift
+++ DisasterAssistance/ViewControllers/ProfileViewController.swift
@@ -9,8 +9,35 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let dataSource = ["Удалить аккаунт", "Выйти из аккаунта"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+        
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 }
